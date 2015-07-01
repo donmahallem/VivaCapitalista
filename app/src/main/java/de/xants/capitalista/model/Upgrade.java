a1f6d0cfe12a694565996f5260152f7d527a44fd
donmahallem <m@m.m0k.eu>
2015-07-01 09:58:30 +0200

    changed cost of upgrades

--- app/src/main/java/de/xants/capitalista/model/Upgrade.java
+++ app/src/main/java/de/xants/capitalista/model/Upgrade.java
@@ -30,23 +30,23 @@ public class Upgrade {
             case 0:
                 return new Upgrade(ProductionType.TYPE1, 10 * Math.pow(2, position));
             case 1:
-                return new Upgrade(ProductionType.TYPE2, 10 * Math.pow(2, position));
+                return new Upgrade(ProductionType.TYPE2, 15 * Math.pow(2, position));
             case 2:
-                return new Upgrade(ProductionType.TYPE3, 10 * Math.pow(2, position));
+                return new Upgrade(ProductionType.TYPE3, 20 * Math.pow(2, position));
             case 3:
-                return new Upgrade(ProductionType.TYPE4, 10 * Math.pow(2, position));
+                return new Upgrade(ProductionType.TYPE4, 35 * Math.pow(2, position));
             case 4:
-                return new Upgrade(ProductionType.TYPE5, 10 * Math.pow(2, position));
+                return new Upgrade(ProductionType.TYPE5, 40 * Math.pow(2, position));
             case 5:
-                return new Upgrade(ProductionType.TYPE6, 10 * Math.pow(2, position));
+                return new Upgrade(ProductionType.TYPE6, 45 * Math.pow(2, position));
             case 6:
-                return new Upgrade(ProductionType.TYPE7, 10 * Math.pow(2, position));
+                return new Upgrade(ProductionType.TYPE7, 50 * Math.pow(2, position));
             case 7:
-                return new Upgrade(ProductionType.TYPE8, 10 * Math.pow(2, position));
+                return new Upgrade(ProductionType.TYPE8, 55 * Math.pow(2, position));
             case 8:
-                return new Upgrade(ProductionType.TYPE9, 10 * Math.pow(2, position));
+                return new Upgrade(ProductionType.TYPE9, 60 * Math.pow(2, position));
             case 9:
-                return new Upgrade(ProductionType.TYPE10, 10 * Math.pow(2, position));
+                return new Upgrade(ProductionType.TYPE10, 65 * Math.pow(2, position));
             default:
                 return null;
         }
